cd74906630a92eb5ab9c1ef86750d83be1248d63
baiyidjp <271524878@qq.com>
2015-08-15 10:22:55 +0800

    Initial Commit

---
+++ 无线滚动/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  无线滚动
+//
+//  Created by I Smile going on 15/8/15.
+//  Copyright (c) 2015年 I Smile going. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 无线滚动/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  无线滚动
+//
+//  Created by I Smile going on 15/8/15.
+//  Copyright (c) 2015年 I Smile going. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
